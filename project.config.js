f43c2d4b14d3554e18663ca5d782fafa60adbfda
yushicheng <1542874601@qq.com>
2020-09-16 01:46:56 +0800

    2020年09月16日 01:56的快速提交

--- project.config.js
+++ project.config.js
@@ -1,11 +1,14 @@
 /* eslint-disable*/
-
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
 module.exports = {
   devServer:{
-
+    port:7000,
+    open:false
   },
   webpackConfig:{
-
+    plugins:[
+      new MomentLocalesPlugin()
+    ]
   }
 };
