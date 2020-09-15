b9e2d30146ab2080ce3f756b32b246780383fd7a
yushicheng <1542874601@qq.com>
2020-09-16 00:53:35 +0800

    2020年09月16日 00:34的快速提交

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 
 import css from "./index.less";
-import images from "./images/8f17419a90.jpg";
 
 
 export default function HomePage() {
@@ -14,7 +13,6 @@ export default function HomePage() {
   });
   return (
     <div>
-      <img className={css["images"]} src={images} width="100%"/>
       <div className={css["home-page"]}>Hello Words</div>
     </div>)
 };
